b7540d759abfbf19f51b321dca10c322c4bb47a0
Mada Upendra <upendravarma1234@gmail.com>
2025-03-12 22:31:05 -0500

    Intial Commit

---
+++ src/main/java/com/upendra/java_daily_tip/JavaDailyTipApplication.java
@@ -0,0 +1,13 @@
+package com.upendra.java_daily_tip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDailyTipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDailyTipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upendra/java_daily_tip/JavaDailyTipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upendra.java_daily_tip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDailyTipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
